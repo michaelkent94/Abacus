26c917cca7b3e8ffb5135a7888e3189c5bb6d98b
Michael Kent <michael.kent@garmin.com>
2016-07-29 07:41:21 -0500

    Initial Commit

---
+++ Abacus/Abacus.h
@@ -0,0 +1,19 @@
+//
+//  Abacus.h
+//  Abacus
+//
+//  Created by Kent, Michael on 7/29/16.
+//  Copyright © 2016 Kent, Michael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Abacus.
+FOUNDATION_EXPORT double AbacusVersionNumber;
+
+//! Project version string for Abacus.
+FOUNDATION_EXPORT const unsigned char AbacusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Abacus/PublicHeader.h>
+
+
